84d46f71c80d7dd3006d75a2707df638fb650d12
Pyoung Kang Kim <pyoungkangkim@gmail.com>
2016-12-10 20:51:26 -0500

    database boilerplate

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
